7ef7d1ce305d7b7a98c0895599dc116cce770a26
Tihon <amandolanders@gmail.com>
2025-12-19 21:34:42 +0500

    'rsafdsafdsa'

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,8 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "proctoring",
-  password: "danikek2008",
-  port: 5432
+  password: "c2900a8bdd",
+  port: 5432,
 });
 
 module.exports = pool;
